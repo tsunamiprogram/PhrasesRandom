729f3a5b9df71a7bc6f69d448627293ed9700a68
brayan gonzalez <maralealesam@gmail.com>
2023-10-03 23:18:15 -0500

    designchange

--- src/components/QuoteCard.jsx
+++ src/components/QuoteCard.jsx
@@ -17,7 +17,7 @@ const QuoteCard = ({quote, setQuote, setCurrentBg, backgrounds}) => {
             <h1 className='quoteCard--title'>Best Famous Quotes of All Times</h1>
             <p className='quoteCard--phrase'>{quote.phrase} </p>
 
-            <button className='quoteCard--btn' onClick={handleChangeQuote}>Change</button>
+            <button className='quoteCard--btn' onClick={handleChangeQuote}> <img className='icon' src='/public/\shuffle.png '/> </button>
         </article>
 
         <footer className='quoteCard--footer'>
